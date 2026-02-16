aa8a776da032863b8d184ca7ce9c51d56df34b51
Ayyappa Gandikota <coder.honey.915@gmail.com>
2026-02-16 22:46:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Wishlist from "./pages/Wishlist";
@@ -7,6 +8,7 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
@@ -17,18 +19,18 @@ function App() {
       <div className="main-content">
         <Routes>
 
-          {/* Default route */}
-          <Route path="/" index element={<Products></Products>} />
+          {/* ✅ Redirect root "/" to "/products" */}
+          <Route path="/" element={<Navigate to="/products" replace />} />
 
+          {/* Main Pages */}
           <Route path="/products" element={<Products />} />
-
           <Route path="/wishlist" element={<Wishlist />} />
-
           <Route path="/cart" element={<Cart />} />
-
           <Route path="/login" element={<Login />} />
-
           <Route path="/register" element={<Register />} />
+
+          {/* Optional: 404 Page (Recommended) */}
+          <Route path="*" element={<h2>Page Not Found</h2>} />
 
         </Routes>
       </div>
